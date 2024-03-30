f26a92732cc25f49c0ebbd372ba9681a89c737d5
Pedro <pedrodalmeida18@gmail.com>
2024-03-30 06:52:47 +0000

    Mon formulaire

---
+++ index.js
@@ -0,0 +1,202 @@
+// // Définition des variables contenant le texte du titre et du paragraphe
+// let contenuTitre = "Azertype"
+// let contenuParagraphe = "L'application pour apprendre à taper plus vite !"
+
+// // Création d'un div avec createElement. Dans cette div, on va créer un titre h1 et un paragraphe p
+// let nouvelleDiv = document.createElement("div")
+// let nouveauTitre = document.createElement("h1")
+// let nouveauParagraphe = document.createElement("p")
+
+// // On ajoute du texte dans le titre et le paragraphe
+// nouveauTitre.textContent = contenuTitre
+// nouveauParagraphe.textContent = contenuParagraphe
+
+// // On ajoute le titre et le paragraphe dans la div
+// nouvelleDiv.appendChild(nouveauTitre)
+// nouvelleDiv.appendChild(nouveauParagraphe)
+
+// // On ajoute la div dans le body
+// let body = document.querySelector("body")
+// body.appendChild(nouvelleDiv)
+
+
+
+// let Contenu = "Fuckest"
+// let content = "Fuckest 2"
+// let maDiv = document.createElement("div")
+
+// let nouveau=document.createElement("h1")
+// let second = document.createElement("p")
+
+// nouveau.textContent=Contenu
+// second.textContent=content
+
+// maDiv.appendChild(nouveau)
+// maDiv.appendChild(second)
+
+// let body = document.querySelector("body")
+
+// body.appendChild(maDiv)
+
+// let contenuTitre = "Azertype"
+// let contenuParagraphe = "L'application pour apprendre à taper plus vite !"
+
+// let div = `
+//     <div>
+//         <h1>${contenuTitre}</h1>
+//         <p>${contenuParagraphe}</p>
+//     </div>
+//     `;
+//     let body = document.querySelector("body")
+//     body.innerHTML = div
+
+
+// let content = "SOCIALIZE IT CENTER"
+// let Contenu="What's SOCIALIZE ?"
+
+// let div =`
+//     <div>
+//         <h1>${Contenu}</h1>
+//         <p>${content}</p>
+//     </div>
+// `
+// let body = document.querySelector("body")
+// body.innerHTML = div
+
+// Définition des variables contenant le texte du titre et du paragraphe
+// let contenuTitre = "Azertype"
+// let contenuParagraphe = "L'application pour apprendre à taper plus vite !"
+
+// // Création d'un div avec createElement. Dans cette div, on va créer un titre h1 et un paragraphe p
+// let nouvelleDiv = document.createElement("div")
+// let nouveauTitre = document.createElement("h1")
+// let nouveauParagraphe = document.createElement("p")
+
+// // On ajoute du texte dans le titre et le paragraphe
+// nouveauTitre.textContent = contenuTitre
+// nouveauParagraphe.textContent = contenuParagraphe
+
+// // On ajoute le titre et le paragraphe dans la div
+// nouvelleDiv.appendChild(nouveauTitre)
+// nouvelleDiv.appendChild(nouveauParagraphe)
+
+// // On ajoute la div dans le body
+// let body = document.querySelector("body")
+// body.appendChild(nouvelleDiv)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let Afficher ="BIGPEDRO THE ONE AND THE ONLY"
+
+// let p = document.createElement("p")
+// p.innerText = Afficher
+
+// let body = document.querySelector("body")
+// body.appendChild(p)
+
+// let nom = "pedro"
+// let prenom = "Silas"
+
+// let maDiv = document.createElement("div")
+// let titre = document.createElement("h1")
+// let paragraphem=document.createElement("p")
+
+// titre.textContent=nom
+// paragraphem.textContent=prenom
+
+// maDiv.appendChild(titre)
+// maDiv.appendChild(paragraphem)
+
+// let body = document.querySelector("body")
+// body.appendChild(maDiv)
+
+// let nom ="ONLY BIG PEDRO"
+// let prenom="BIG MAN"
+
+// let div=`
+//     <div>
+//     <h1>${nom}</h1>
+//     <p>${prenom}</p>
+// `
+
+// let body = document.querySelector("body")
+// body.innerHTML = div
+
+
+
+
+
+
+
+
+
+
+// let nom ="ONLY BIG PEDRO"
+// let prenom="BIG MAN"
+
+// let div=`
+// <div>
+// <h1>${nom}</h1>
+// <p>${prenom}</p>
+// </div>
+// `
+// let body = document.querySelector("body")
+// body.innerHTML = div
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let nom ="ONLY BIG PEDRO"
+// let prenom="BIG MAN"
+
+// let h1 = document.createElement("h1")
+// let p=document.createElement("p")
+// let div = document.createElement("div")
+
+// h1.innerText =prenom
+// p.innerText =nom
+
+// div.appendChild(h1)
+// div.appendChild(p)
+
+// let body = document.querySelector("body")
+// body.appendChild(div)
+
+
+
+
+
+let titre="Le titre de ma page"
+
+let h1=document.createElement("h1")
+h1.innerText = titre
+
+let body = document.querySelector("body")
+body.appendChild(h1)
